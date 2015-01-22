dfe053295591d15eacc53e670184018bd7c57d65
Luaancz <Luaancz@users.noreply.github.com>
2015-01-22 20:39:33 +0100

    Added automatic settings upgrade.

--- SalemOptimizer/Program.cs
+++ SalemOptimizer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SalemOptimizer.Properties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace SalemOptimizer
         [STAThread]
         static void Main()
         {
+            Settings.Default.Upgrade();
+            Settings.Default.Save();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
