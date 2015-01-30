1f4f413659dc17e2dfa7f52a5c58bd5d1b173d4a
Luaancz <Luaancz@users.noreply.github.com>
2015-01-31 00:02:17 +0100

    Changed the version.

--- SalemOptimizer/Properties/AssemblyInfo.cs
+++ SalemOptimizer/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
-[assembly: AssemblyInformationalVersion("30. 1. 2015")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyInformationalVersion("31. 1. 2015")]
