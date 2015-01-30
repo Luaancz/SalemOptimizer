297125b047371a388ce55eae3c6ee8cd64baa9c1
Luaancz <Luaancz@users.noreply.github.com>
2015-01-30 09:00:44 +0100

    Inspirational id is now taken *after* the list is filtered. This gets rid of an issue with IndexOutOfRangeException.

--- SalemOptimizer/InspirationalDatabase.cs
+++ SalemOptimizer/InspirationalDatabase.cs
@@ -16,27 +16,27 @@ namespace SalemOptimizer
 
         static List<Inspirational> Load()
         {
-            return
-                File.ReadAllLines("Inspirationals.tab")
-                    .Select(row => row.Split('\t'))
-                    .Select
-                    (
-                        (cols, index) =>
-                            new Inspirational
-                            {
-                                Id = index,
-                                Name = cols[0],
-                                Proficiencies =
-                                    cols
-                                        .Select((val, idx) => new { Index = idx, Value = val })
-                                        .Where(i => i.Index >= 2 && i.Index <= 16 && !string.IsNullOrWhiteSpace(i.Value))
-                                        .ToDictionary(i => (ProficiencyKind)(i.Index - 2), i => int.Parse(i.Value)),
-                                Uses = int.Parse(cols[18]),
-                                Weight = string.IsNullOrWhiteSpace(cols[1]) ? 1d : double.Parse(cols[1], CultureInfo.InvariantCulture)
-                            }
-                    )
-                    .Where(i => i.Proficiencies.Count > 0)
-                    .ToList();
+			return
+				File.ReadAllLines("Inspirationals.tab")
+					.Select(row => row.Split('\t'))
+					.Select
+					(
+						(cols, index) =>
+							new Inspirational
+							{
+								Name = cols[0],
+								Proficiencies =
+									cols
+										.Select((val, idx) => new { Index = idx, Value = val })
+										.Where(i => i.Index >= 2 && i.Index <= 16 && !string.IsNullOrWhiteSpace(i.Value))
+										.ToDictionary(i => (ProficiencyKind)(i.Index - 2), i => int.Parse(i.Value)),
+								Uses = int.Parse(cols[18]),
+								Weight = string.IsNullOrWhiteSpace(cols[1]) ? 1d : double.Parse(cols[1], CultureInfo.InvariantCulture)
+							}
+					)
+					.Where(i => i.Proficiencies.Count > 0)
+					.Select((inspirational, index) => { inspirational.Id = index; return inspirational; })
+					.ToList();
         }
     }
 }
